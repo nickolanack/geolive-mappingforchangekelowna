e70eed906b21444bdbf17c78cc97d0ba5979f820
Nicholas  Blackwell <nblackwe@Nicholass-Mac-Pro.local>
2016-10-18 11:44:37 -0700

    autofmt

--- widget.SidePanel.14.customScript.js
+++ widget.SidePanel.14.customScript.js
@@ -1,22 +1,34 @@
-var detailContent=container.appendChild(new Element('div', {'class':'detail-content'}));
-var tabs=container.appendChild(new Element('ul',{'class':'sidebar-tabs'}));
-var page=container.appendChild(new Element('div', {'class':'sidebar-content'}));
+var detailContent = container.appendChild(new Element('div', {
+	'class': 'detail-content'
+}));
+var tabs = container.appendChild(new Element('ul', {
+	'class': 'sidebar-tabs'
+}));
+var page = container.appendChild(new Element('div', {
+	'class': 'sidebar-content'
+}));
 
 
 
-var infoTabContent=page.appendChild(new Element('div', {'class':'infotab-content'}));
-var editTabContent=page.appendChild(new Element('div', {'class':'edittab-content'}));
-var agencyTabContent=page.appendChild(new Element('div', {'class':'agencytab-content'}));
+var infoTabContent = page.appendChild(new Element('div', {
+	'class': 'infotab-content'
+}));
+var editTabContent = page.appendChild(new Element('div', {
+	'class': 'edittab-content'
+}));
+var agencyTabContent = page.appendChild(new Element('div', {
+	'class': 'agencytab-content'
+}));
 
-var ContentModuleViewer=new Class({
-	Extends:ModuleViewer,
-	initialize:function(map, container, options){
-		var me=this;
-		me._container=container
+var ContentModuleViewer = new Class({
+	Extends: ModuleViewer,
+	initialize: function(map, container, options) {
+		var me = this;
+		me._container = container
 		me.parent(map, options);
 	},
-	display:function(content){
-		var me=this;
+	display: function(content) {
+		var me = this;
 		me._container.empty();
 		me._container.appendChild(content);
 
@@ -25,95 +37,119 @@ var ContentModuleViewer=new Class({
 	}
 });
 
-var miniDetailViewController=new ContentModuleViewer(map, detailContent, {});
-var infoTabViewController=new ContentModuleViewer(map, infoTabContent, {});
-var editTabViewController=new ContentModuleViewer(map, editTabContent, {});
-var agencyTabViewController=new ContentModuleViewer(map, agencyTabContent, {});
+var miniDetailViewController = new ContentModuleViewer(map, detailContent, {});
+var infoTabViewController = new ContentModuleViewer(map, infoTabContent, {});
+var editTabViewController = new ContentModuleViewer(map, editTabContent, {});
+var agencyTabViewController = new ContentModuleViewer(map, agencyTabContent, {});
 
-var tabNames=[];
-var tabEls={
-}
-var setTab=function(n, e){
+var tabNames = [];
+var tabEls = {}
+var setTab = function(n, e) {
 
-	tabNames.forEach(function(n){
+	tabNames.forEach(function(n) {
 		page.removeClass(n);
 		tabEls[n].removeClass('active');
 	});
 
 	page.addClass(n);
 	tabEls[n].addClass('active');
-	if(tabNames.indexOf(n)<0){
+	if (tabNames.indexOf(n) < 0) {
 		tabNames.push(n);
 	}
 };
 
-var hideTab=function(n){
+var hideTab = function(n) {
 	tabEls[n].addClass('hidden');
 }
 
-var showTab=function(n){
+var showTab = function(n) {
 	tabEls[n].removeClass('hidden');
 }
 
 
 
-var info=tabs.appendChild(new Element('li',{'html':'info', events:{click:function(){
-	setTab('info');
-}}}));
-var edit=tabs.appendChild(new Element('li',{'html':'edit', events:{click:function(){
-	setTab('edit');
-}}}));
-var agency=tabs.appendChild(new Element('li',{'html':'agency', events:{click:function(){
-	setTab('agency');
-}}}));
+var info = tabs.appendChild(new Element('li', {
+	'html': 'info',
+	events: {
+		click: function() {
+			setTab('info');
+		}
+	}
+}));
+var edit = tabs.appendChild(new Element('li', {
+	'html': 'edit',
+	events: {
+		click: function() {
+			setTab('edit');
+		}
+	}
+}));
+var agency = tabs.appendChild(new Element('li', {
+	'html': 'agency',
+	events: {
+		click: function() {
+			setTab('agency');
+		}
+	}
+}));
 
-tabEls={
-	info:info,
-	edit:edit,
-	agency:agency
+tabEls = {
+	info: info,
+	edit: edit,
+	agency: agency
 };
 
 setTab('info');
 
-var isAgency=function(marker){
-	return (marker.getLayer().getId()===2);
+var isAgency = function(marker) {
+	return (marker.getLayer().getId() === 2);
 }
-var agencyLayer=function(){
+var agencyLayer = function() {
 	return map.getLayerManager().getLayer(2);
 }
 
-var displayAgencyFor=function(mapitem, userHasWriteAccess){
+var displayAgencyFor = function(mapitem, userHasWriteAccess) {
 
 	(new GetAttributeItemValueQuery(
 		mapitem.getId(), mapitem.getType(), 'serviceProviderAttributes', 'agency'
 	)).addEvent("success", function(result) {
 
 		//defualt numeric value is 0. but is mapped to an id. 
-		var agencyId=parseInt(result.value);
-		var agencyMapitem=null;
-		if(agencyId>0){
-			agencyMapitem=map.getLayerManager().filterMapitemById(agencyId, null, {layer:agencyLayer()});
+		var agencyId = parseInt(result.value);
+		var agencyMapitem = null;
+		if (agencyId > 0) {
+			agencyMapitem = map.getLayerManager().filterMapitemById(agencyId, null, {
+				layer: agencyLayer()
+			});
 		}
 
-		if(agencyMapitem){
-			agencyTabViewController.open(new GeoliveTemplateModule(map, agencyMapitem, {template:"default", page:"AgencyDetail"}), mapitem);
-		}else{
+		if (agencyMapitem) {
+			agencyTabViewController.open(new GeoliveTemplateModule(agencyMapitem, {
+				template: "default",
+				page: "AgencyDetail"
+			}), mapitem);
+		} else {
 
-			if(userHasWriteAccess){
+			if (userHasWriteAccess) {
 				var wizardTemplate = (map.getDisplayController().getWizardTemplate('SetAgencyWizardTemplate'));
 				if ((typeof wizardTemplate) != 'function') {
-				    return false;
+					return false;
 				}
 
 				var wizard = wizardTemplate(mapitem, {});
 
 				var step = wizard.build(map, agencyTabViewController, {}); //'geolive' string is for css
 				step();
-			}else{
+			} else {
 
 
 
-				agencyTabViewController.open(new TemplateModule(map, {content:[new HTMLModule(map, 'No agency has been selected for this service provider')]}, {template:"default", page:"AgencyDetail"}), mapitem);
+				agencyTabViewController.open(new TemplateModule({
+					content: [new HTMLModule(map, 'No agency has been selected for this service provider')]
+				}, {
+					template: "default",
+					page: "AgencyDetail"
+				}), mapitem);
 			}
 
 		}
@@ -126,17 +162,17 @@ var displayAgencyFor=function(mapitem, userHasWriteAccess){
 
 map.setItemEditFn(function(mapitem, options) {
 
-	var me=this;
+	var me = this;
 
 	if (!(mapitem instanceof GeoliveMapItem)) {
-	    throw new Error('MapFactory.MarkerWizard expects instance of GeoliveMapItem');
+		throw new Error('MapFactory.MarkerWizard expects instance of GeoliveMapItem');
 	}
 
 
 
-	var wizardTemplate = (map.getDisplayController().getWizardTemplate(isAgency(mapitem)?'AgencyWizardTemplate':'ServiceProviderWizardTemplate'));
+	var wizardTemplate = (map.getDisplayController().getWizardTemplate(isAgency(mapitem) ? 'AgencyWizardTemplate' : 'ServiceProviderWizardTemplate'));
 	if ((typeof wizardTemplate) != 'function') {
-	    return false;
+		return false;
 	}
 
 	var wizard = wizardTemplate(mapitem, {});
@@ -148,44 +184,50 @@ map.setItemEditFn(function(mapitem, options) {
 
 });
 
-var lastMapitem=null;
+var lastMapitem = null;
 map.setMarkerClickFn(function(mapitem) {
 
-    /* globals GeoliveTemplateModule */
-    if (!window.GeoliveTemplateModule) {
-        throw Error('Requires GeoliveTemplateModule class');
-    }
+	/* globals GeoliveTemplateModule */
+	if (!window.GeoliveTemplateModule) {
+		throw Error('Requires GeoliveTemplateModule class');
+	}
 
-    infoTabViewController.open(new GeoliveTemplateModule(map, mapitem,{template:"default", page:isAgency(mapitem)?"AgencyDetail":"ServiceProviderDetail"}), mapitem);
-    miniDetailViewController.open(new GeoliveTemplateModule(map, mapitem,{template:"default", page:"Detail"}), mapitem);
-    
-    hideTab('agency');
+	infoTabViewController.open(new GeoliveTemplateModule(mapitem, {
+		template: "default",
+		page: isAgency(mapitem) ? "AgencyDetail" : "ServiceProviderDetail"
+	}), mapitem);
+	miniDetailViewController.open(new GeoliveTemplateModule(mapitem, {
+		template: "default",
+		page: "Detail"
+	}), mapitem);
 
-    GeoliveClient.authorize('write', mapitem, function(userHasWriteAccess) {
+	hideTab('agency');
 
-    	if(lastMapitem){
-    		MapFactory.DisableMouseEditing(lastMapitem);
-    	}
-    	lastMapitem=mapitem;
-    	MapFactory.EnableMouseEditing(mapitem);
+	GeoliveClient.authorize('write', mapitem, function(userHasWriteAccess) {
+
+		if (lastMapitem) {
+			MapFactory.DisableMouseEditing(lastMapitem);
+		}
+		lastMapitem = mapitem;
+		MapFactory.EnableMouseEditing(mapitem);
 
 
-    	if(!isAgency(mapitem)){
-	    	showTab('agency');
-	    	displayAgencyFor(mapitem, userHasWriteAccess);
-	    }
+		if (!isAgency(mapitem)) {
+			showTab('agency');
+			displayAgencyFor(mapitem, userHasWriteAccess);
+		}
 
-	    if (userHasWriteAccess) {
-	    	map.editItem(mapitem, {});
-	    	showTab('edit');
-	    	setTab('edit');
-	    }else{
-	    	hideTab('edit');
+		if (userHasWriteAccess) {
+			map.editItem(mapitem, {});
+			showTab('edit');
+			setTab('edit');
+		} else {
+			hideTab('edit');
 			setTab('info');
-	    }
-	    	
-	    show();
-	    
+		}
+
+		show();
+
 	});
 
 });
