63f57e24c3d290b7339a99dbebd868b3215f746d
nickolanack <nickblackwell82@gmail.com>
2016-10-22 14:20:21 -0700

    auto commit message

--- widget.WizardBuilder.15.metadata.children.1.builderMetadata.positions.content.modules.9.config.initializationScript.js
+++ widget.WizardBuilder.15.metadata.children.1.builderMetadata.positions.content.modules.9.config.initializationScript.js
@@ -1,35 +1,85 @@
-var showHousingType=function(type){
+var showHousingType = function(type) {
 
-var hide=[
- 'housingTypeStudioBachelorNumberOfUnits'
-];
- var emergencyHouseing=[
- 'housingTypeStudioBachelorNumberOfUnits'
-];
-var transitionalHousing=[];
 
-var typeMap={
- 'emergency':emergencyHouseing,
- 'transitional':transitionalHousing
-};
+  var emergencyHouseing = [
+    'housingTypeDormitoryNumberOfBeds',
+    'housingTypeSharedBedroomNumberOfBeds',
+    'housingTypeBedroomNumberOfBeds',
+  ];
+  var transitionalHousing = [
+    'housingTypeSharedBedroomNumberOfBeds',
+    'housingTypeStudioBachelorNumberOfUnits',
+    'housingType1BedroomNumberOfUnits',
+    'housingType2BedroomNumberOfUnits',
+    'housingType3BedroomNumberOfUnits',
+    'housingType4BedroomNumberOfUnits',
+    'housingTypeOtherBedroomNumberOfUnits',
+    'housingTypeOtherBedroomDescription'
 
-var map=typeMap[type];
+  ];
+  var supportiveHousing = [
+    'housingTypeHouseSharedBedroomNumberOfBeds',
+    'housingTypeHouseIndividualBedroomNumberOfBeds',
+    'housingTypeApartmentStudioNumberOfUnits',
+    'housingTypeApartmen1BedroomNumberOfUnits',
+    'housingTypeApartmen2BedroomNumberOfUnits',
+    'housingTypeOtherNumberOfUnits',
+    'housingTypeOtherDescription'
 
-   moduleGroup.forEach(function(object, i){
-      var module=object._module;
-       if(i>moduleIndex&&module){
-         var id=module.getIdentifier();
-         if(id){
+  ];
+  var nonpHousing = [
+    'housingTypeHouseSharedBedroomNumberOfBeds',
+    'housingTypeHouseIndividualBedroomNumberOfBeds',
+    'housingTypeSecondaryHouseSuiteNumberOfUnits',
+    'housingTypeStudioBachelorNumberOfUnits',
+    'housingType1BedroomNumberOfUnits',
+    'housingType2BedroomNumberOfUnits',
+    'housingType3BedroomNumberOfUnits',
+    'housingType4BedroomNumberOfUnits',
+    'housingTypeOtherBedroomNumberOfUnits',
+    'housingTypeOtherBedroomDescription'
 
-             if((!map)||map.indexOf(id)>=0){
-                  module.show();
-              }else if(hide.indexOf(id)>=0){
-                  module.hide();
-              }
-         }
-       }
-   });
+  ];
+  var rentalHousing = [
+    //missing house shared accom
+    'housingTypeSecondaryHouseSuiteNumberOfUnits',
+    'housingTypeApartmentStudioNumberOfUnits',
+    'housingTypeApartmen1BedroomNumberOfUnits',
+    'housingTypeApartmen2BedroomNumberOfUnits',
+    'housingTypeDuplexNumberOfUnits',
+    'housingTypeFourplexNumberOfUnits',
+    'housingTypeTownHouseNumberOfUnits',
+    'housingTypeOtherNumberOfUnits',
+    'housingTypeOtherDescription'
 
+  ];
+
+  var hide = ([]).concat(emergencyHouseing, transitionalHousing, supportiveHousing, nonpHousing, rentalHousing);
+
+  var typeMap = {
+    'emergency': emergencyHouseing,
+    'transitional': transitionalHousing,
+    'supportive': supportiveHousing,
+    'nonprofit': nonpHousing,
+    'rental': rentalHousing
+  };
+
+  var map = typeMap[type];
+
+  moduleGroup.forEach(function(object, i) {
+    var module = object._module;
+    if (i > moduleIndex && module) {
+      var id = module.getIdentifier();
+      if (id) {
+
+        if ((!map) || map.indexOf(id) >= 0) {
+          module.show();
+        } else if (hide.indexOf(id) >= 0) {
+          module.hide();
+        }
+      }
+    }
+  });
 
 
 
