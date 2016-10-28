2fb3b35300ee763b8bdecb7f1a27efd556688804
nickolanack <nickblackwell82@gmail.com>
2016-10-28 12:54:42 -0700

    auto commit message

---
+++ app/widget.ReusableModuleGroup.51.metadata.positions.content.modules.1.config.initializationScript.js
@@ -0,0 +1,50 @@
+var valuesArray = [
+
+	"Male",
+	"Female",
+	"Transgender",
+
+
+];
+var fieldName='genderServed';
+WizardHelper.declareAggregateFunctionObjectForStep(stepIndex, position, moduleIndex, "Attribute_serviceProviderAttributes_Object");
+WizardHelper.addModuleDataAggregateFunctionForStep(stepIndex, position, moduleIndex, function(object, attributes) {
+	attributes[fieldName]= object.values;
+});
+
+var addValue = function(nameOrIndex) {
+
+	var name = nameOrIndex;
+	if ((typeof nameOrIndex) === 'number') {
+		name = valuesArray[nameOrIndex];
+	}
+
+	if (!object.values) {
+		object.values = [];
+	}
+
+	var i = object.values.indexOf(name);
+	if (i < 0) {
+		object.values.push(name);
+	}
+
+}
+
+
+var removeValue = function(nameOrIndex) {
+
+	var name = nameOrIndex;
+	if ((typeof nameOrIndex) === 'number') {
+		name = valuesArray[nameOrIndex];
+	}
+
+	if (!object.values) {
+		object.values = [];
+	}
+
+	var i = object.values.indexOf(name);
+	if (i >= 0) {
+		object.values.splice(i, 1);
+	}
+
+}
