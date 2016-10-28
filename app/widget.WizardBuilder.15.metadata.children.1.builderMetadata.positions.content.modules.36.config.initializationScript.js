f764d0a4d959e9983b04acad9097f88c628f0ab5
nickolanack <nickblackwell82@gmail.com>
2016-10-28 12:03:06 -0700

    auto commit message

--- app/widget.WizardBuilder.15.metadata.children.1.builderMetadata.positions.content.modules.36.config.initializationScript.js
+++ app/widget.WizardBuilder.15.metadata.children.1.builderMetadata.positions.content.modules.36.config.initializationScript.js
@@ -1,7 +1,53 @@
-servicesProvided
+var servicesArray = [
+
+	"Case Management",
+	"Referrals to other Agencies",
+	"Health/Dental Care",
+	"Meals",
+	"Showers",
+	"Storage",
+	"Clothing, Household Goods"
+
+];
 
 WizardHelper.declareAggregateFunctionObjectForStep(stepIndex, position, moduleIndex, "Attribute_serviceProviderAttributes_Object");
-WizardHelper.addModuleDataAggregateFunctionForStep(stepIndex, position, moduleIndex,  function(object, Attribute_serviceProviderAttributes_Object){
+WizardHelper.addModuleDataAggregateFunctionForStep(stepIndex, position, moduleIndex, function(object, attributes) {
+	attributes.servicesProvided = object.values;
+});
 
-			    		            	Attribute_serviceProviderAttributes_Object.servicesProvided=object.values;
-			    		            });
+var addService = function(nameOrIndex) {
+
+	var name = nameOrIndex;
+	if ((typeof nameOrIndex) === 'number') {
+		name = servicesArray[nameOrIndex];
+	}
+
+	if (!object.values) {
+		object.values = [];
+	}
+
+	var i = object.values.indexOf(name);
+	if (i < 0) {
+		object.values.push(name);
+	}
+
+}
+
+
+var removeService = function(nameOrIndex) {
+
+	var name = nameOrIndex;
+	if ((typeof nameOrIndex) === 'number') {
+		name = servicesArray[nameOrIndex];
+	}
+
+	if (!object.values) {
+		object.values = [];
+	}
+
+	var i = object.values.indexOf(name);
+	if (i >= 0) {
+		object.values.splice(i, 1);
+	}
+
+}
