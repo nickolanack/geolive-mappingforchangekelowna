e7cf19be20bb0b340ebf11a758957da61809c7cf
nickolanack <apache@s54.ok.ubc.ca>
2016-11-29 13:57:48 -0800

    auto commit message

--- app/widget.WizardBuilder.25.metadata.children.1.builderMetadata.positions.content.modules.0.config.onRenderTextFieldScript.js
+++ app/widget.WizardBuilder.25.metadata.children.1.builderMetadata.positions.content.modules.0.config.onRenderTextFieldScript.js
@@ -1,13 +1,13 @@
-var search = new UISearchControl(inputElement,{});
-                            UISearchControl.AddAggregatedSearchResults(search, [
+var search = new UISearchControl(inputElement, {});
+UISearchControl.AddAggregatedSearchResults(search, [
 
-                                new MarkerSearchAggregator(map, search,{
-							header:"Agencies",
-                                                       ResultClickEventFunction:function(result){
-textField.setValue(result.id);
-//alert("boom - you blew up the internet");
-                                                       }
-					}),
+	new MarkerSearchAggregator(map, search, {
+		header: "Agencies",
+		ResultClickEventFunction: function(result) {
+			textField.setValue(result.id);
+			//alert("boom - you blew up the internet");
+		}
+	}),
 
 
-                            ],{});
+], {});
