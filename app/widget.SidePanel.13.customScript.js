c652c3953623138c22a65be81859919d8821738a
nickolanack <apache@s54.ok.ubc.ca>
2016-12-02 15:39:08 -0800

    auto commit message

---
+++ app/widget.SidePanel.13.customScript.js
@@ -0,0 +1,9 @@
+var layersFilterContent = container.appendChild(new Element('ul', {
+	'class': 'layer-filter'
+}));
+var housingTypesFilterContent = container.appendChild(new Element('ul', {
+	'class': 'housing-filter'
+}));
+var servicesTypesFilterContent = container.appendChild(new Element('ul', {
+	'class': 'services-filter'
+}));
