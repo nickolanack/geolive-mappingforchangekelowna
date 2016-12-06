fa1017b669b885fb418b0dacbcde54279eb270b0
nickolanack <apache@s54.ok.ubc.ca>
2016-12-06 09:56:56 -0800

    auto commit message

--- app/widget.SidePanel.13.customScript.js
+++ app/widget.SidePanel.13.customScript.js
@@ -1,9 +1,55 @@
-var layersFilterContent = container.appendChild(new Element('ul', {
-	'class': 'layer-filter'
+var layersFilterContent = container.appendChild(new Element('div', {
+	'class': 'filter layer-filter'
 }));
-var housingTypesFilterContent = container.appendChild(new Element('ul', {
-	'class': 'housing-filter'
+var housingTypesFilterContent = container.appendChild(new Element('div', {
+	'class': 'filter housing-filter'
 }));
-var servicesTypesFilterContent = container.appendChild(new Element('ul', {
-	'class': 'services-filter'
+var servicesTypesFilterContent = container.appendChild(new Element('div', {
+	'class': 'filter services-filter'
 }));
+
+var layersMod=<?php
+Core::Modules();
+$module = Module::LoadModule('LayerLegend', array());
+echo $module->display($targetInstance->getJSObjectName(), $targetInstance->getJSObjectName(), 'map', array(
+   "iconSize"=>32,
+   "shouldShowLayerScript"=>'
+  
+        return id!=4;
+
+   '
+
+
+));
+?>;
+layersMod.load(null, layersFilterContent, null);
+
+
+var getAttributeFilterParameters=function(field, table){
+ 
+   return {};
+
+};
+
+
+var attributesMod=<?php
+$module = Module::LoadModule('plugin.Attributes.AttributeFilterGroup', array());
+echo $module->display($targetInstance->getJSObjectName(), $targetInstance->getJSObjectName(), 'map', array(
+    "preferredSortOrder"=>array(
+        "buildingType", 
+        "servicesProvided", 
+        "otherServices",
+       "primaryTargetResident", 
+       "genderServed", 
+       "demographicServed"
+    ),
+  "fieldBlackList"=>array("email"),
+  "tableBlackList"=>array("agencyAttributes"),
+  "moduleOptionsScript"=>'
+    
+       return getAttributeFilterParameters(field, table);
+
+  '
+));
+?>;
+attributesMod.load(null, housingTypesFilterContent, null);
