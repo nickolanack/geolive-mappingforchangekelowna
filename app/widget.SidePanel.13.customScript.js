6e5a35094cab7ae8fe4a272f5edd912b9fb9448e
nickolanack <apache@s54.ok.ubc.ca>
2016-12-06 11:29:30 -0800

    auto commit message

--- app/widget.SidePanel.13.customScript.js
+++ app/widget.SidePanel.13.customScript.js
@@ -27,27 +27,41 @@ layersMod.load(null, layersFilterContent, null);
 
 var getAttributeFilterParameters=function(field, table){
  
- if(field=='buildingType'){
+//TODO: use global icons sets instead of duplicating icons here, and in wizard forms
+
+  var iconizeFilters={
+
+    buildingType:[
+      "components/com_geolive/users_files/user_files_994/Uploads/fik_Qih_[ImAgE]_[G]_7qp.png",
+      "components/com_geolive/users_files/user_files_994/Uploads/[G]_[ImAgE]_Pts_qTq_2cO.png",
+      "components/com_geolive/users_files/user_files_994/Uploads/[ImAgE]_eI5_Wj9_9fx_[G].png",
+      "components/com_geolive/users_files/user_files_994/Uploads/YdL_[G]_5ID_t0q_[ImAgE].png",
+      "components/com_geolive/users_files/user_files_994/Uploads/[ImAgE]_W0M_lWp_zQJ_[G].png"
+    ]
+
+
+  }
+
+
+  if(iconizeFilters[field]){
 
     return {
       listTemplate:function(values){
 
-        var me=this; //bound to filter object
+        //Rendering an icon selection ui instead of 
+
+        var me=this; //bound to Attributes filter object
+
         var div=new Element('div');
         var iconSelection=new UIIconizedSelectionControl(div, {
 
-          icons: [
-              "components/com_geolive/users_files/user_files_994/Uploads/fik_Qih_[ImAgE]_[G]_7qp.png",
-              "components/com_geolive/users_files/user_files_994/Uploads/[G]_[ImAgE]_Pts_qTq_2cO.png",
-              "components/com_geolive/users_files/user_files_994/Uploads/[ImAgE]_eI5_Wj9_9fx_[G].png",
-              "components/com_geolive/users_files/user_files_994/Uploads/YdL_[G]_5ID_t0q_[ImAgE].png",
-              "components/com_geolive/users_files/user_files_994/Uploads/[ImAgE]_W0M_lWp_zQJ_[G].png"
-          ],
-
+          icons: iconizeFilters[field],
         });
+
         iconSelection.addEvent('loadIcon', function(icon, i, asset){
           me._addMouseEventsForFilterItem(asset, values[i]);
         });
+
 
 
 
@@ -60,6 +74,7 @@ var getAttributeFilterParameters=function(field, table){
 
   }
 
+ 
    return {};
 
 };
