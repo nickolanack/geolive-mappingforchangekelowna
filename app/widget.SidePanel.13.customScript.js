6fd72140e82ea5d65f96a4748787e87f0dc09a6d
nickolanack <apache@s54.ok.ubc.ca>
2016-12-06 10:57:47 -0800

    auto commit message

--- app/widget.SidePanel.13.customScript.js
+++ app/widget.SidePanel.13.customScript.js
@@ -27,6 +27,39 @@ layersMod.load(null, layersFilterContent, null);
 
 var getAttributeFilterParameters=function(field, table){
  
+ if(field=='buildingType'){
+
+    return {
+      listTemplate:function(values){
+
+        var me=this; //bound to filter object
+        var div=new Element('div');
+        var iconSelection=new UIIconizedSelectionControl(div, {
+
+          icons: [
+              "components/com_geolive/users_files/user_files_994/Uploads/fik_Qih_[ImAgE]_[G]_7qp.png",
+              "components/com_geolive/users_files/user_files_994/Uploads/[G]_[ImAgE]_Pts_qTq_2cO.png",
+              "components/com_geolive/users_files/user_files_994/Uploads/[ImAgE]_eI5_Wj9_9fx_[G].png",
+              "components/com_geolive/users_files/user_files_994/Uploads/YdL_[G]_5ID_t0q_[ImAgE].png",
+              "components/com_geolive/users_files/user_files_994/Uploads/[ImAgE]_W0M_lWp_zQJ_[G].png"
+          ],
+
+        });
+        iconSelection.addEvent('loadIcon', function(icon, i, asset){
+          me._addMouseEventsForFilterItem(asset, values[i]);
+        });
+
+
+
+        return div;
+
+
+      }
+
+    };
+
+  }
+
    return {};
 
 };
