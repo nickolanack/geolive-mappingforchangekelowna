61eb42c57dad2478491d8dc061bda0739493b49a
nickolanack <nickblackwell82@gmail.com>
2017-02-27 12:44:32 -0800

    robo commit message

---
+++ app/Drop in Services Search.customScript.js
@@ -0,0 +1,107 @@
+<section class="explore drop-in-services">
+<h1>Drop In Services</h1>
+
+<?php
+
+
+
+
+HtmlBlock('article',
+            array(
+                'class'=>'fav-1',
+                'heading'     => 2,
+                'schema'      => array(
+                    'link' => 'itemprop="Service"',
+                ),
+                'afterHeader' => '<hr/>',
+                'title'=>'Looking for drop-in services?',
+                'text'=>'Click on the button above to find where you can access a meal, laundry services or storage facilities.',
+                'link'=>'/map/layer-1',
+                'linkTitle'=>'',             
+
+'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/z45_[ImAgE]_eLw_[G]_ZBa.png')
+
+
+            ));
+HtmlBlock('article',
+            array(
+                'class'=>'fav-2',
+                'heading'     => 2,
+                'schema'      => array(
+                    'link' => 'itemprop="Service"',
+                ),
+                'afterHeader' => '<hr/>',
+                'title'=>'Do you need family housing?',
+                'text'=>'Click on the button above if you have a child and need to access housing.',
+                 'link'=>'/map/filter-primary-target-resident-equal-to-families',
+                 'linkTitle'=>'',  'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/RF8_U7a_[G]_StY_[ImAgE].png')
+
+
+            ));
+HtmlBlock('article',
+            array(
+                'class'=>'fav-3',
+                'heading'     => 2,
+                'schema'      => array(
+                    'link' => 'itemprop="Service"',
+                ),
+                'afterHeader' => '<hr/>',
+                 'title'=>'Would you like a hot shower?',
+                'text'=>'Click on the button above to find out where to drop-in for a hot shower in Kelowna?',
+                 'link'=>'/map/filter-services-provided-equal-to-showers',
+                 'linkTitle'=>'',  
+
+'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/QbA_[G]_[ImAgE]_eWR_rXc.png')
+
+
+            ));
+
+HtmlBlock('article',
+            array(
+                'class'=>'fav-3',
+                'heading'     => 2,
+                'schema'      => array(
+                    'link' => 'itemprop="Service"',
+                ),
+                'afterHeader' => '<hr/>',
+                 'title'=>'Would you like a hot shower?',
+                'text'=>'Click on the button above to find out where to drop-in for a hot shower in Kelowna?',
+                 'link'=>'/map/filter-services-provided-equal-to-showers',
+                 'linkTitle'=>'',  
+
+'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/QbA_[G]_[ImAgE]_eWR_rXc.png')
+
+
+            ));
+
+
+
+HtmlBlock('article',
+            array(
+                'class'=>'fav-3',
+                'heading'     => 2,
+                'schema'      => array(
+                    'link' => 'itemprop="Service"',
+                ),
+                'afterHeader' => '<hr/>',
+                 'title'=>'Would you like a hot shower?',
+                'text'=>'Click on the button above to find out where to drop-in for a hot shower in Kelowna?',
+                 'link'=>'/map/filter-services-provided-equal-to-showers',
+                 'linkTitle'=>'',  
+
+'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/QbA_[G]_[ImAgE]_eWR_rXc.png')
+
+
+            ));
+
+
+
+
+
+
+
+
+
+?>
+
+</section>
--- app/Selection Housing Search.customScript.js
+++ app/Selection Housing Search.customScript.js
@@ -1,5 +1,5 @@
 <section class="housing-search">
-     <p><form>
+     <p><div class="housing-search-area" ><form id="housing-search-form" action="/map">
 
 <label>What kind of housing are you looking for?</label>
    
@@ -10,14 +10,21 @@
 <select class="attributeFilter" data-attribute-field="primaryTargetResident" data-attribute-table="serviceProviderAttributes"></select>
 <select class="attributeFilter" data-attribute-field="genderServed" data-attribute-table="serviceProviderAttributes"></select>
 <select class="attributeFilter" data-attribute-field="demographicServed" data-attribute-table="serviceProviderAttributes"></select>
-<label>Items that match all fields<input id="join-intersect" type="radio" name="join" value="intersect"  checked="true" /></label>
-<label>Items that match any field<input type="radio" name="join" value="join" / ></label>
-  </form></p>
-</section>
-<section class="housing-results empty">
-<h3><span id="housing-results">0 Results </span>for your search<h3>
-<h4 >you searched <span id="housing-query">...</span><h4>
-<p></p>
+
+</form></p>
+<label>Items that match all fields<input class="attributeFilterJoin" id="join-intersect" type="radio"  name="join" value="intersect"  checked="true" /></label>
+<label>Items that match any field<input class="attributeFilterJoin" id="join-intersect" type="radio" name="join" value="join" / ></label>
+  
+ 
+  <section class="housing-results empty well" id="housing-results-section">
+  <h3><span id="housing-results">0 Results</span> for your search</h3>
+  <p><h4>you searched </h4> <span id="housing-query">...</span>
+  </p>
+    <button class="btn btn-primary" id="housing-search-goto-map">Show Results</button>
+  </section>
+
+  </div> 
+
 </section>
   
 <?php
@@ -43,7 +50,56 @@ window.addEvent("load",function(){
         });
         
        var selects=[];
-       var countResults=function(){
+
+       var setResultCount=function(count, link){
+
+            if(count<=0){
+              $$(".housing-results button").forEach(function(b){
+                  b.addClass("disabled");
+              });
+
+             
+              if(count==-1){
+                 $("housing-results").innerHTML="Looking";
+                 $("housing-results-section").removeClass("empty");
+              }else{
+                 $("housing-results").innerHTML="0 Results";
+                 $("housing-results-section").addClass("empty");
+              }
+               console.log("remove events");
+              $("housing-search-goto-map").removeEvents();
+
+            }else{
+              $$(".housing-results button").forEach(function(b){
+                  b.removeClass("disabled");
+              });
+
+               $("housing-results").innerHTML="<span class=\"r-count\">"+count+"</span> Result"+(count==1?"":"s");
+
+               console.log("add events");
+               var url="map/filter-"+link;
+               $("housing-search-goto-map").setAttribute("title",url);
+               $("housing-search-goto-map").addEvent("click", function(){
+
+                $("housing-search-form").action=url;
+                 $("housing-search-form").submit();
+
+               });
+
+            }
+
+
+          };
+
+           var setSearchedFor=function(label){
+              $("housing-query").innerHTML=label
+
+           }
+
+          setResultCount(0);
+          setSearchedFor("...");
+
+       var queryResults=function(){
 
           var filters=[];
 
@@ -58,35 +114,48 @@ window.addEvent("load",function(){
           });
 
           
-          var join=$("join-intersect").checked?"intersect":"join"
-          var searchQuery=new FilterSearchQuery({
-            filters:{"join":join,"table":"serviceProviderAttributes","set":"*","filters":filters},
-            mapId:1
-          });
-          searchQuery.addEvent("onSuccess",function(response){
+          
+          if(filters.length){
 
-            var results=response.results;
-            console.log(results);
-           $("housing-results").innerHTML=results.countMatches+"Results";
-           $("housing-query").innerHTML=results.link.description
+            setResultCount(-1);
+            setSearchedFor("...")
 
-          });
-          searchQuery.execute();
+            var join=$("join-intersect").checked?"intersect":"join"
+            var searchQuery=new FilterSearchQuery({
+              filters:{"join":join,"table":"serviceProviderAttributes","set":"*","filters":filters},
+              mapId:1
+            });
+            searchQuery.addEvent("onSuccess",function(response){
+
+              var results=response.results;
+              console.log(results);
+              setResultCount(results.countMatches, results.link.alias);
+              setSearchedFor(results.link.description);
+
+            });
+            searchQuery.execute();
+          }else{
+            setResultCount(0);
+            setSearchedFor("...");
+          }
 
 
        };
  
+        $$(".attributeFilterJoin").forEach(function(el){
+           el.addEvent("change", queryResults);
+        });
   
          $$(".attributeFilter").forEach(function(el){
              selects.push(el);
-             el.addEvent("change", countResults);
+             el.addEvent("change", queryResults);
                var uniqueValuesQuery = new AjaxControlQuery(CoreAjaxUrlRoot, "distinct_attribute_value_list",                {
                 table: el.getAttribute("data-attribute-table"),
                 field: el.getAttribute("data-attribute-field"),
                 plugin: "Attributes"
             }).addEvent("onSuccess", function(distinct) {
                
-                  el.appendChild(new Element("option", {value:"", html:distinct.label, disabled:true}));
+                  el.appendChild(new Element("option", {value:"", html:distinct.label, disabled:true, selected:true}));
                   el.appendChild(new Element("option", {value:"", html:""}));
                   distinct.values.filter(function(value){
                        return !!(value&&value!="");
