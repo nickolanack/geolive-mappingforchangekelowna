0859fb06dcc5221a19c7da22f29324520b41661f
nickolanack <nickblackwell82@gmail.com>
2017-02-27 14:18:04 -0800

    robo commit message

--- app/Quick Start.customScript.js
+++ app/Quick Start.customScript.js
@@ -1,5 +1,5 @@
 <section class="quickstart">
-    <h1>How To Get Started</h1>
+   <article> <h1>How To Get Started</h1>
     <p>By answering a few quick questions, i-Search Kelowna can filter the housing options available and provide a
 more targeted list of appropriate options.</p>
-</section>
+<article></section>
