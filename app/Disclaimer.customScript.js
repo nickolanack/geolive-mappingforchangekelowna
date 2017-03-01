f2b00c4fcfdb77d0a4cd7ee9355efb76279c0457
nickolanack <nickblackwell82@gmail.com>
2017-03-01 14:33:23 -0800

    robo commit message

---
+++ app/Disclaimer.customScript.js
@@ -0,0 +1,13 @@
+<section><p><span class="bold">Disclaimer:</span> Built as a collaboration between community agencies, funders and researchers from the
+
+University of British Columbia and Okanagan College, i-Search Kelowna aims to reduce the time
+
+experienced when investigating housing options in the area. i-Search Kelowna does not place individuals
+
+in housing and appearance on this site should not be considered an endorsement of service. Agencies
+
+have provided information on how they accept and place individuals and the services they offer.
+
+However, they update information, so it is recommended that you contact individual agencies for
+
+specific program information.</p></section>
---
+++ app/Housing Definitions.customScript.js
@@ -0,0 +1,108 @@
+<section class="explore housing-types">
+<h1>Housing Definitions</h1>
+
+<div class="width-helper"><?php
+
+
+
+
+HtmlBlock('article',
+            array(
+                'class'=>'fav-3',
+                'heading'     => 2,
+                'schema'      => array(
+                    'link' => 'itemprop="Service"',
+                ),
+                'afterHeader' => '<hr/>',
+                'title'=>'Emergency Housing',
+                'text'=>'provides shelter and accommodation for short periods of time and serves people who are homeless, displaced, or are fleeing violence or abuse.',
+                'link'=>'/map/filter-services-provided-equal-to-Showers',
+                'linkTitle'=>'',             
+
+'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/fik_Qih_[ImAgE]_[G]_7qp.png')
+
+
+            ));
+HtmlBlock('article',
+            array(
+                'class'=>'fav-5',
+                'heading'     => 2,
+                'schema'      => array(
+                    'link' => 'itemprop="Service"',
+                ),
+                'afterHeader' => '<hr/>',
+                'title'=>'Transitional Housing',
+                'text'=>'Offers a bridge between emergency and permanent housing, often for a specified time period. Transitional housing may serve people leaving homelessness, people leaving the corrections system, or families receiving specialized supports.',
+                 'link'=>'/map/filter-services-provided-equal-to-Hygiene',
+                 'linkTitle'=>'',  
+                 'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/[G]_[ImAgE]_Pts_qTq_2cO.png')
+
+
+            ));
+HtmlBlock('article',
+            array(
+                'class'=>'fav-2',
+                'heading'     => 2,
+                'schema'      => array(
+                    'link' => 'itemprop="Service"',
+                ),
+                'afterHeader' => '<hr/>',
+                 'title'=>'Supportive Housing',
+                'text'=>'permanent housing for people who need assistance (personal support services, medical assistance, counselling) to live independently. Supportive housing may house people who are elderly or who have disabilities, addictions, or mental illness.',
+                 'link'=>'/map/filter-services-provided-equal-to-Harm-Reduction',
+                 'linkTitle'=>'',  
+
+'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/[ImAgE]_eI5_Wj9_9fx_[G].png')
+
+
+            ));
+
+HtmlBlock('article',
+            array(
+                'class'=>'fav-1',
+                'heading'     => 2,
+                'schema'      => array(
+                    'link' => 'itemprop="Service"',
+                ),
+                'afterHeader' => '<hr/>',
+                 'title'=>'Low Income Rental Housing',
+                'text'=>'Rental housing for low and moderate income individuals and families, built by the province, a municipality, or by a community group. Most tenants pay rent that’s geared to their income, other pay rent that’s at the low end of private market rent.',
+                 'link'=>'/map/filter-services-provided-equal-to-Harm-Reduction',
+                 'linkTitle'=>'',  
+
+'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/YdL_[G]_5ID_t0q_[ImAgE].png')
+
+
+            ));
+
+
+
+HtmlBlock('article',
+            array(
+                'class'=>'fav-4',
+                'heading'     => 2,
+                'schema'      => array(
+                    'link' => 'itemprop="Service"',
+                ),
+                'afterHeader' => '<hr/>',
+                 'title'=>'Private Market Purpose-Built Rental Housing',
+                'text'=>'Housing that’s been built for the purpose of renting to tenants is called ‘purpose-built rental.’ Apartment buildings are a common type of purpose-built rental. The housing is owned by a individual or a private company and may be managed by the owner or by a hired property manager. Units vary widely based on location, age and condition of building. The owner of the property sets the amount to be paid for rent based on the local housing market.',
+                 'link'=>'/map/filter-services-provided-equal-to-Meals',
+                 'linkTitle'=>'',  
+
+'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/[ImAgE]_W0M_lWp_zQJ_[G].png')
+
+
+            ));
+
+
+
+
+
+
+
+
+
+?>
+</div>
+</section>
