ce8c872e1e83bb7fcbefae443bb9d8eb2f27c366
nickolanack <nickblackwell82@gmail.com>
2017-03-10 11:01:15 -0800

    robo commit message

---
+++ app/Map Configuration Graph.customScript.js
@@ -0,0 +1,20 @@
+<?php
+
+//Edit Links
+UI('widget.button.canvas', 108);
+
+HtmlBlock('widget', 111);
+
+
+HtmlBlock('widget',  112);
+HtmlBlock('widget', 113);
+HtmlBlock('widget',  114);
+HtmlBlock('widget', 115);
+
+
+//Drop Down Selection Search
+HtmlBlock('widget', 88);
+
+
+
+?>
