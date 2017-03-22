e11aae7bca51d3cea4777f77c8f0a327d4f43847
nickolanack <nickblackwell82@gmail.com>
2017-03-22 16:16:17 -0700

    corbett -- Auto Commit - Changes made through Frontend Interface

--- app/Agency Articles.customScript.js
+++ app/Agency Articles.customScript.js
@@ -14,26 +14,26 @@ A login is required in order to add your housing resource to the mapping tool or
 
 
 <a name="Creating"></a><section class="explore drop-in-services">
-<h1>Creating your initial information</h1>
+<h1>Creating or updating your initial information</h1>
 <div class="width-helper"><p>
 
-To review and revise your agency's information, you will need to log in to the site.
+To review and revise your agency's information, you will need to log in to the site.<br></br>
 
-Please use the user name and password provided. If you do not have this information please email mappingforchangekelowna@gmail.com and, if you are a involved in the sector, we will send you this information.
+Please use the user name and password provided. If you do not have this information please email mappingforchangekelowna@gmail.com and, if you are a involved in the sector, we will send you this information.<br></br>
 
-If you hover the 'Map' menu item at the top of the page a pop up called 'my items' will appear, select this. 
+If you hover the 'Map' menu item at the top of the page a pop up called 'my items' will appear, select this. <br></br>
 
-If it does not appear, go to the search bar and type in the name of your agency/program. select your agency/program from the drop down list that will appear. 
+If it does not appear, go to the search bar and type in the name of your agency/program. select your agency/program from the drop down list that will appear. <br></br>
 
-Your information will then appear on the right hand side of the map. 
+Your information will then appear on the right hand side of the map. <br></br>
 
-Ensure the edit tab is engaged. 
+Ensure the edit tab is engaged. <br></br>
 
-Work your way down the form (it is quite long!) to ensure all the information is accurate, revising where it is not. 
+Work your way down the form (it is quite long!) to ensure all the information is accurate, revising where it is not. <br></br>
 
-At the bottom, remember to click on the save button.
+At the bottom, remember to click on the save button.<br></br>
 
-Your information is now updated! Thank you! :)
+Your information is now updated! Thank you! :)<br></br>
 </p></div>
 </section>
 
