bccb8649b15d2f1c035747f733ae78f7059fc62d
nickolanack <nickblackwell82@gmail.com>
2017-03-27 10:07:47 -0700

    nickola -- Auto Commit - Changes made through Frontend Interface

--- app/Front Intro.customScript.js
+++ app/Front Intro.customScript.js
@@ -1,4 +1,4 @@
 <section class="intro">
 
-    <p>Designed to assist individuals looking for low-income rentals, supportive housing or emergency shelters, this website helps pinpoint housing and the agencies that provide it, within the City of Kelowna.<br/><br/><button onclick="window.scrollHelper('GetStarted')" href="#GetStarted" class="btn btn-primary">Get Started</button><button onclick="window.scrollHelper('DropInServices')" href="#DropInServices" class="btn btn-primary">Drop In Services</button><br/> </p>
+    <p>Designed to assist individuals looking for low-income rentals, supportive housing or emergency shelters, this website helps pinpoint housing and the agencies that provide it, within the City of Kelowna.<br/><br/><button onclick="window.scrollHelper('GetStarted')" href="#GetStarted" class="btn btn-primary">Get Started</button><button onclick="window.scrollHelper('DropInServices')" href="#DropInServices" class="btn btn-primary">Drop In Services</button><br/><br/> </p>
 </section>
