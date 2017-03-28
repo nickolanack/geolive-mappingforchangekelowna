a46f2f88728dda0217307f894ebb9062ba2643c7
nickolanack <nickblackwell82@gmail.com>
2017-03-28 14:51:40 -0700

    corbett -- Auto Commit - Changes made through Frontend Interface

--- app/Technical Issue Article.customScript.js
+++ app/Technical Issue Article.customScript.js
@@ -2,12 +2,9 @@
 <h1>Experiencing a Technical Issue</h1>
 <div class="width-helper"><p>
 
-Occasionally, we may experience a technical issue with the site. If that happens, we would appreciate knowing about it. Please fill out the following form to submit a technical issue.
+Occasionally, we may experience a technical issue with the site. If that happens, we would appreciate knowing about it. Please fill out the form below to submit a technical issue.<br/><br/>
 
-To report a Technical Issue with the site
-Thank you for taking the time to report a technical issue. Please fill out the form below to send a report to our technical team. 
-
-It may be helpful to contact you for more detail relating to the issue you are identifying, if you agree to be contacted, please
+It may be helpful to contact you for more detail relating to the issue you are identifying, if you agree to be contacted, please include your email address.<br/><br/>
 </p></div>
 <br/>
 <div class="width-helper">
