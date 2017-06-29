9bb860ae866552896b315f05d63053ff3da02c33
nickolanack <nickblackwell82@gmail.com>
2017-06-29 16:20:51 -0700

    corbett -- Auto Commit - Changes made through Frontend Interface

--- app/Help Sections.customScript.js
+++ app/Help Sections.customScript.js
@@ -25,7 +25,7 @@ Step 1: Click on the advance search icon - the red button with three lines on th
 
 Step 2: Select the icons that match the criteria you are searching for (if you hover over the icons with your mouse you will have a short description). Your search will determine which markers then appear on the map.<br/><br/>
 
-Step 3: Using the buttons "Items that match all fields" and  "Items that match any field" you can set your filters to include ALL of the search items or JUST the search items that are contained in all of the filtered markers.
+Step 3: Using the buttons "Items that match all fields" and  "Items that match any field" you can set your filters to include ALL of the search items or JUST the search items that are contained in all of the filtered markers.<br/><br/>
 
 This tool is built on google maps. You can drag the map around using a mouse, zoom in and out, and set your maps default view on full screen using the buttons on the right hand side of the map.<br/><br/>
 
