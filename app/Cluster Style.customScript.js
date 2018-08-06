60ac786e94f69477ea537012189b8f7ffaf5097c
nickolanack <nickblackwell82@gmail.com>
2018-08-06 13:01:09 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Cluster Style.customScript.js
+++ app/Cluster Style.customScript.js
@@ -5,6 +5,8 @@ if (window.Cluster) {
 	};
 	ClusterSymbol.IconStyle = function(name) {
 
+    
+
 		var colorsForLayer = {
 			"1":'rgb(31,120,180)',
 			"2":'rgb(223,194,125)',
@@ -20,7 +22,7 @@ if (window.Cluster) {
 		var getColor = function(colorMap,
 			defaultColor) {
 			try {
-				var lid = me.cluster_.markers_[0].wrapper.getLayer().getId();
+				var lid = me.cluster_.markers_[0]._layerid
 				if (colorMap['' + lid]) {
 					return colorMap['' + lid];
 				}
