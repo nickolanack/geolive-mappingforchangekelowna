1eaa316285350e8eee1c842126516736141afe47
nickolanack <nickblackwell82@gmail.com>
2018-08-07 15:04:34 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Services Form.resolveItemScript.js
+++ app/Services Form.resolveItemScript.js
@@ -1,4 +1,20 @@
-<?php IncludeExternalJS("https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places"); ?>
+<?php 
+
+$apikey=GetPlugin('Maps')->getParameter('googleMapsServerApiKey', '');
+        if(!empty($apikey)){
+            $apikey='&key='.$apikey;
+        }
+        
+        $version=GetPlugin('Maps')->getParameter('googleMapsVersion', '');
+        if(!empty($version)){
+            $version='&v='.$version;
+        }
+
+        IncludeExternalJS(
+            '//maps.google.com/maps/api/js?libraries=geometry,places,weather,visualization&callback=GoogleState.setGoogleLoaded'.$apikey.$version,
+        );
+
+?>
 
 return new MockDataTypeItem({
     "id":-1,
