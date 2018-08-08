c9670f29331345ab7c8f96bfe8f4ac06d5849ad0
nickolanack <nickblackwell82@gmail.com>
2018-08-07 22:50:16 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/services-map-widgets.customScript.js
@@ -0,0 +1,17 @@
+<?php
+
+
+foreach(Widget::GetWidgetsForTarget('map.1') as $widget){
+				if(($widget->getEnabled()) && Auth('read', $widget, 'widget')){
+				    
+				    
+				    
+				    
+					$widget->display($map);
+				}
+				
+			}
+
+
+
+?>
