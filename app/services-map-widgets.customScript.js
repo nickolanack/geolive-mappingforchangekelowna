1e5a9053cea270742abdc8d5122f1fadaeab8292
nickolanack <nickblackwell82@gmail.com>
2018-08-07 22:52:46 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/services-map-widgets.customScript.js
+++ app/services-map-widgets.customScript.js
@@ -5,7 +5,9 @@ foreach(Widget::GetWidgetsForTarget('map.1') as $widget){
 				if(($widget->getEnabled()) && Auth('read', $widget, 'widget')){
 				    
 				    
-				    
+				    if(class_exists('SidePanel')&&$widget instanceof SidePanel){
+				        continue;
+				    }
 				    
 					$widget->display($targetInstance);
 				}
