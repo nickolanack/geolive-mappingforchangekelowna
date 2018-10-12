e5836216d3dd9486c9aa002dd4a1db81bf41e37b
nickolanack <nickblackwell82@gmail.com>
2018-10-12 10:48:49 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/inline-agency-form.onRenderApplictionScript.js
@@ -0,0 +1,19 @@
+HtmlDocument()->META(HtmlDocument()->website(), 'base');
+GetWidget('form-styles')->display($targetInstance);
+GetWidget('attributeAndIconsetVariables')->display($targetInstance);
+
+
+
+$apikey=GetPlugin('Maps')->getParameter('googleMapsServerApiKey', '');
+        if(!empty($apikey)){
+            $apikey='&key='.$apikey;
+        }
+        
+        $version=GetPlugin('Maps')->getParameter('googleMapsVersion', '');
+        if(!empty($version)){
+            $version='&v='.$version;
+        }
+
+        IncludeExternalJS(
+            '//maps.google.com/maps/api/js?libraries=places'.$apikey.$version
+        );
