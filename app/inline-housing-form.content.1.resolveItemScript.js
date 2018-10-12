0a51ac763da0cfc55b6d6d1f1bf3871c831f9deb
nickolanack <nickblackwell82@gmail.com>
2018-10-12 10:14:41 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/inline-housing-form.content.1.resolveItemScript.js
+++ app/inline-housing-form.content.1.resolveItemScript.js
@@ -2,7 +2,7 @@
 return new MockDataTypeItem({
     "id":-1,
     "type":"marker",
-   // "latLng":{"lat":49.870536, "lng":-119.463176},
+    //"latLng":{"lat":49.870536, "lng":-119.463176},
     "title":"",
     "description":""
 });
