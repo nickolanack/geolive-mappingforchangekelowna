65d5968fc60ef2a68de3815ad1dd496b384bcc78
nickolanack <nickblackwell82@gmail.com>
2018-10-18 08:04:52 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/inline-services-form.content.1.resolveItemScript.js
+++ app/inline-services-form.content.1.resolveItemScript.js
@@ -1,4 +1,13 @@
-return new MockDataTypeItem({
+
+var MockMapitem=new Class({
+    Extends:MockDataTypeItem,
+    save:function(cb){
+        
+        cb(false);
+    }
+})
+
+return new MockMapitem({
     "id":-1,
     "type":"marker",
    // "latLng":{"lat":49.870536, "lng":-119.463176},
