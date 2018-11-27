0c9114d0c1c82626364de59b8d3ee5a1883541e6
nickolanack <nickblackwell82@gmail.com>
2018-11-26 16:35:23 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/Services Left Panel Filters.customScript.js
@@ -0,0 +1,56 @@
+<?php
+ GetPlugin('Isearch')->includeScripts();
+?>
+sidePanel.addEvent('show:once', function() {
+	var filterBuilder = new IsearchFilterBuilder(map, container, {
+	    rangeFilters : function(){ return {
+			estimatedWaitTime: {},
+			MonthlyCostOfStay: {},
+			maxDurationOfStay: {}
+
+
+		}; },
+
+	    iconizeFilters : function(){ return {
+
+			buildingType: window["BuildingTypeIcons"],
+			servicesProvided: window["ServicesProvidedIcons"],
+			genderServed: window["GenderServedIcons"],
+			primaryTargetResident: window["PrimaryTargetResidentIcons"]
+		}; },
+
+		attributeList:function(){ return window.attributeValueList; }
+
+	    
+	    
+	    
+	});
+	var attributesMod = <?php
+
+
+
+    $whiteList=array(
+        "buildingType", 
+        "servicesProvided", 
+        "primaryTargetResident", 
+        "genderServed", 
+        "demographicServed"
+
+    );
+
+
+$module = Module::LoadModule('plugin.Attributes.AttributeFilterGroup', array());
+echo $module->display($targetInstance->getJSObjectName(), $targetInstance->getJSObjectName(), 'map', array(
+    "preferredSortOrder"=>$whiteList,
+    "fieldWhiteList"=>$whiteList,
+
+  "moduleOptionsScript"=>'
+    
+       return filterBuilder.getAttributeFilterParameters(field, table);
+
+  '
+));
+?>;
+
+	attributesMod.load(null, filterBuilder.getElement(), null);
+});
