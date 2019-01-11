43bd45c874288c5053f4b92326f6d9be2b1b2d4f
nickolanack <nickblackwell82@gmail.com>
2019-01-10 22:13:40 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Services Form.1.content.15.onRenderTextFieldScript.js
+++ app/Services Form.1.content.15.onRenderTextFieldScript.js
@@ -3,8 +3,13 @@ module.runOnceOnLoad(function(){
                       
                         moduleGroup.forEach(function(mod){
                             if(mod._module.getIdentifier()=="geolocationField"){
+                                
+                                var mirror=false;
                             	mod._module.addEvent("change",function(){
                             	    if(module.getValue()==""&&mod._module.getValue()&&mod._module.getValue()!=""){
+                            	        mirror=true;
+                            	    }
+                            	    if(mirror){
                             	        module.setValue(mod._module.getValue());
                             	    }
                             	})
