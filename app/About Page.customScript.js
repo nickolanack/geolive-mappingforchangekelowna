393bd1a7f74827bec8b95c5271515a86c0135ac9
nickolanack <nickblackwell82@gmail.com>
2019-01-16 11:14:49 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/About Page.customScript.js
+++ app/About Page.customScript.js
@@ -2,8 +2,7 @@
 
 GetWidget('frontPageStyle')->display();
 GetWidget('frontArticleStyle')->display();
-//GetWidget('frontPageStyle')->display();
 
-HtmlBlock('widget', 98);
+GetWidget('aboutSectionContent')->display();
 
 ?>
