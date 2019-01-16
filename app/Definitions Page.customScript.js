70b863a19889ee4aee8fde11d4fcd7b5ca8c2659
nickolanack <nickblackwell82@gmail.com>
2019-01-16 11:40:14 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Definitions Page.customScript.js
+++ app/Definitions Page.customScript.js
@@ -1,7 +1,7 @@
 <?php
 GetWidget('frontPageStyle')->display();
 GetWidget('frontArticleStyle')->display();
-//GetWidget('90')->display();
+GetWidget('housingDefinitionsContent')->display();
 GetWidget('redZoneContent')->display();
 GetWidget('scrollHelperScript')->display();
 
