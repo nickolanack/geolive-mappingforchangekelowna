56b58b6b29b05e9ff1b9538c7b49e85f7231d3ae
nickolanack <nickblackwell82@gmail.com>
2019-01-17 11:41:18 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Housing Definitions.customScript.js
+++ app/Housing Definitions.customScript.js
@@ -16,7 +16,7 @@ HtmlBlock('article',
                 'afterHeader' => '<hr/>',
                 'title'=>'Emergency',
                 'text'=>'Provides shelter and accommodation for short periods of time and serves people who are homeless, displaced, or are fleeing violence or abuse.',
-                'link'=>'/map/filter-services-provided-equal-to-Showers',
+                'link'=>'/map/filter-building-type-equal-to-emergency',
                 'linkTitle'=>'',             
 
 'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/fik_Qih_[ImAgE]_[G]_7qp.png')
@@ -33,7 +33,7 @@ HtmlBlock('article',
                 'afterHeader' => '<hr/>',
                 'title'=>'Transitional',
                 'text'=>'Offers a bridge between emergency and permanent housing, often for a specified time period. Transitional housing may serve people leaving homelessness, people leaving the corrections system, or families receiving specialized supports.',
-                 'link'=>'/map/filter-services-provided-equal-to-Hygiene',
+                 'link'=>'/map/filter-building-type-equal-to-transitional',
                  'linkTitle'=>'',  
                  'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/[G]_[ImAgE]_Pts_qTq_2cO.png')
 
@@ -49,7 +49,7 @@ HtmlBlock('article',
                 'afterHeader' => '<hr/>',
                  'title'=>'Supportive',
                 'text'=>'Permanent housing for people who need assistance (personal support services, medical assistance, counselling) to live independently. Supportive housing may house people who are elderly or who have disabilities, addictions, or mental illness.',
-                 'link'=>'/map/filter-services-provided-equal-to-Harm-Reduction',
+                 'link'=>'/map/filter-building-type-equal-to-supportive',
                  'linkTitle'=>'',  
 
 'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/[ImAgE]_eI5_Wj9_9fx_[G].png')
@@ -67,7 +67,7 @@ HtmlBlock('article',
                 'afterHeader' => '<hr/>',
                  'title'=>'Low Income Rental',
                 'text'=>'Rental housing for low and moderate income individuals and families, built by the province, a municipality, or by a community group. Most tenants pay rent that’s geared to their income, other pay rent that’s at the low end of private market rent.',
-                 'link'=>'/map/filter-services-provided-equal-to-Harm-Reduction',
+                 'link'=>'/map/filter-building-type-equal-to-low-income-rental',
                  'linkTitle'=>'',  
 
 'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/3I1_sMs_[ImAgE]_[G]_mbr.png')
@@ -87,7 +87,7 @@ HtmlBlock('article',
                 'afterHeader' => '<hr/>',
                  'title'=>'Market Rental',
                 'text'=>'Housing that’s been built for the purpose of renting to tenants is called ‘purpose-built rental.’ Apartment buildings are a common type of purpose-built rental. The housing is owned by a individual or a private company and may be managed by the owner or by a hired property manager. Units vary widely based on location, age and condition of building. The owner of the property sets the amount to be paid for rent based on the local housing market.',
-                 'link'=>'/map/filter-services-provided-equal-to-Meals',
+                 'link'=>'/map/filter-building-type-equal-to-rental',
                  'linkTitle'=>'',  
 
 'images'=>array('https://isearchkelowna.ca/components/com_geolive/users_files/user_files_994/Uploads/[ImAgE]_W0M_lWp_zQJ_[G].png')
