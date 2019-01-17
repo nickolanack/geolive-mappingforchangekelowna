45b5149889d0abb24701426e271e41b866d0133a
nickolanack <nickblackwell82@gmail.com>
2019-01-17 12:26:18 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/widget.CustomBehaviorScript.150.customScript.js
+++ app/widget.CustomBehaviorScript.150.customScript.js
@@ -2,7 +2,7 @@ var element=new Element('div');
 
 var Control=new UIMapControl(map, {
 				element:element,
-				anchor:'top',
+				anchor:'tm',
 				className:'top-filter-form'
 			});
 			Control.addControl();
