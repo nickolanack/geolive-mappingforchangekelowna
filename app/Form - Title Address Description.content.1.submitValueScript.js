d9e585e2f46344a3682bb95adc7a2f0fdb201cf3
nickolanack <nickblackwell82@gmail.com>
2019-01-16 18:03:34 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Form - Title Address Description.content.1.submitValueScript.js
+++ app/Form - Title Address Description.content.1.submitValueScript.js
@@ -1,1 +1,1 @@
-wizardDataSet.address=object.value; if(object.value){item.setLatLng(object.value)};
+wizardDataSet.address=object.value; if(object.value&&(object.value.lat||object.value instanceof google.maps.LatLng)){item.setLatLng(object.value)};
