1c3670b6255f0d9b9f7ba3222ae5fc3d9ca34246
nickolanack <nickblackwell82@gmail.com>
2019-01-22 01:08:39 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/inline-daily-form.content.1.resolveItemScript.js
+++ app/inline-daily-form.content.1.resolveItemScript.js
@@ -1,8 +1,3 @@
 
-return new MockDataTypeItem({
-    "id":-1,
-    "type":"marker",
-    //"latLng":{"lat":49.870536, "lng":-119.463176},
-    "title":"",
-    "description":""
-});
+return IsearchFormFilters.GetMockItem(
+    5, 'components/com_geolive/users_files/user_files_994/Uploads/[G]_[ImAgE]_fIR_hWi_8e5.png');
