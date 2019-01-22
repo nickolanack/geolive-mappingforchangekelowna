616408aafc69cdc8dcb46780c960d8579de57b61
nickolanack <nickblackwell82@gmail.com>
2019-01-22 00:58:41 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/inline-housing-form.onRenderApplictionScript.js
+++ app/inline-housing-form.onRenderApplictionScript.js
@@ -1,5 +1,6 @@
 HtmlDocument()->META(HtmlDocument()->website(), 'base');
 GetWidget('form-styles')->display($targetInstance);
+IncludeJS("{plugins}/Isearch/js/IsearchFormFilters.js");
 GetWidget('attributeAndIconsetVariables')->display($targetInstance);
 
 
